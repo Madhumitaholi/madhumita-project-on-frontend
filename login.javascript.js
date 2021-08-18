7ec8810bff8796b44ca19f3f5ce08132a7862505
Madhumitaholi <88303234+Madhumitaholi@users.noreply.github.com>
2021-08-19 03:00:16 +0530

    javascript
    Implementation of login page

---
+++ login.javascript.js
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>Login and Registration Form Design</title>
+<link rel="stylesheet" href="style.css"></link>
+</head>
+<body>
+<div class="login-page">
+<div class="form">
+<form class="register-form">
+<input type="text" placeholder="user name"/>
+<input type="text" placeholder="password"/>
+<input type="text" placeholder="email id"/>
+<button>Create</button>
+<p class="message">Already Registered? <a href="#">Log in</a>
+</p>
+<form class="login-form">
+<input type="text" placeholder="user name"/>
+<input type="text" placeholder="pass word"/>
+<button>Login</button>
+<p class="message">Not Registered? <a href="#">Register</a></p>
+</form>
+</div>
+</div>
+</body>
+</html>
